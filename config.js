8c080aad7438870d06a2fd195e3e6071eb9b87b5
juzkaf52 <juzkaf52@gmail.com>
2026-04-18 16:45:53 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js — Shared across all pages
-const API_URL = 'https://script.google.com/macros/s/AKfycbwUZYUkT0xhACA4GL9NV2j7p9yr5vO8WRbW3giqQpiqLsXUFjp_soujk1v3kCwOFJTv_w/exec'; // Replace with your deployed URL
+const API_URL = 'https://script.google.com/macros/s/AKfycbxXR9nKM9smeBBHS4hwSuTXkEXjtuVJHguVJ6iBqtI9uwS2RblyVO8OvyDrffDvMJTiuA/exec'; // Replace with your deployed URL
 
 async function api(action, payload = {}) {
   const res = await fetch(API_URL, {
